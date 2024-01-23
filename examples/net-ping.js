3c7892154cb6bb2fb39b3b1f28166ede3666acde
Dante C <49130056+dantectl@users.noreply.github.com>
2024-01-23 17:11:00 -0500

    Update net-ping.js

--- examples/net-ping.js
+++ examples/net-ping.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 var ping = require("net-ping");
 var session = ping.createSession ();
 
+// this nodejs module only works with ip addresses. enter the ip for your host in the target variable below
 var target = "ip.address.here";
 
 
